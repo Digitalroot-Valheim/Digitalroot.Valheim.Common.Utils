7bd734440601079ac4cf096c98407cf8547269aa
Digitalroot <Digitalroot@users.noreply.github.com>
2021-07-22 22:07:19 -0700

    Update GuiUtils.cs

--- src/Digitalroot.Valheim.Common.Utils/GuiUtils.cs
+++ src/Digitalroot.Valheim.Common.Utils/GuiUtils.cs
@@ -1,13 +1,13 @@
 ﻿namespace Digitalroot.Valheim.Common
 {
-  internal static class GuiUtils
+  public static class GuiUtils
   {
-    internal static void PrintToCenterOfScreen(string msg)
+    public static void PrintToCenterOfScreen(string msg)
     {
       Player.m_localPlayer.Message(MessageHud.MessageType.Center, msg);
     }
 
-    internal static void PrintToTopLeftOfScreen(string msg)
+    public static void PrintToTopLeftOfScreen(string msg)
     {
       Player.m_localPlayer.Message(MessageHud.MessageType.TopLeft, msg);
     }
