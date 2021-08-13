b7ef6ba6b21cc16909308026cd4f4f1a02da10e4
Nicholas Dunnaway <nick@dunnaway.org>
2021-08-12 21:21:58 -0700

    Added more utils

--- src/Digitalroot.Valheim.Common.Utils/Utils.cs
+++ src/Digitalroot.Valheim.Common.Utils/Utils.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using UnityEngine;
 
 namespace Digitalroot.Valheim.Common
 {
@@ -45,6 +46,9 @@ namespace Digitalroot.Valheim.Common
     public static string Namespace => $"Digitalroot.Valheim.{nameof(Common)}";
 
     [UsedImplicitly]
+    public static List<T> AllOf<T>() => Enum.GetValues(typeof(T)).OfType<T>().ToList();
+
+    [UsedImplicitly]
     public static IEnumerable<string> AllNames(Type type)
     {
       var f = type.GetFields().Where(f1 => f1.FieldType == typeof(string));
@@ -53,6 +57,27 @@ namespace Digitalroot.Valheim.Common
         yield return fieldInfo.GetValue(null).ToString();
       }
     }
+
+    [UsedImplicitly]
+    public static bool DoesPluginExist(string pluginGuid) => Chainloader.PluginInfos.Any(keyValuePair => keyValuePair.Value.Metadata.GUID == pluginGuid);
+
+    [UsedImplicitly]
+    public static Vector3 GetGroundHeight(int x, int z) => GetGroundHeight(new Vector3Int(x, 500, z));
+
+    [UsedImplicitly]
+    public static Vector3 GetGroundHeight(float x, float z) => GetGroundHeight(new Vector3(x, 500, z));
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static Vector3 GetGroundHeight(Vector3Int vector3) => new(vector3.x, ZoneSystem.instance.GetGroundHeight(vector3), vector3.z);
+
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static Vector3 GetGroundHeight(Vector3 vector3) => new(vector3.x, ZoneSystem.instance.GetGroundHeight(vector3), vector3.z);
+
+    [UsedImplicitly]
+    public static Vector3 GetLocalPlayersPosition() => Player.m_localPlayer.transform.position;
+
+    [UsedImplicitly]
+    public static string GetPluginPath(Type modPluginType) => Path.GetDirectoryName(modPluginType.Assembly.Location);
 
     [UsedImplicitly]
     public static T GetPrivateField<T>(object instance, string name)
@@ -75,7 +100,7 @@ namespace Digitalroot.Valheim.Common
 
       if (method == null)
       {
-        Type[] types = args == null ? new Type[0] : args.Select(arg => arg.GetType()).ToArray();
+        Type[] types = args == null ? Type.EmptyTypes : args.Select(arg => arg.GetType()).ToArray();
         method = instance.GetType().GetMethod(name, types);
       }
 
@@ -87,6 +112,9 @@ namespace Digitalroot.Valheim.Common
 
       return method.Invoke(instance, args);
     }
+
+    [UsedImplicitly]
+    public static bool IsGameInMainScene() => ZNetScene.instance != null;
 
     [UsedImplicitly]
     public static bool IsObjectDBReady()
@@ -124,6 +152,18 @@ namespace Digitalroot.Valheim.Common
       return Localization.instance.Localize(value);
     }
 
+    public static Vector3 GetStartTemplesPosition()
+    {
+      if (ZoneSystem.instance.FindClosestLocation("StartTemple", Vector3.zero, out ZoneSystem.LocationInstance locationInstance))
+      {
+        Log.Trace(Logger, $"[GetStartTemplesPosition] StartTemple at {locationInstance.m_position}");
+        return locationInstance.m_position;
+      }
+      Log.Error(Logger, $"[GetStartTemplesPosition] Can't find StartTemple");
+
+      return Vector3.zero;
+    }
+
     [UsedImplicitly]
     public static void SetPrivateField(object instance, string name, object value)
     {
@@ -138,16 +178,18 @@ namespace Digitalroot.Valheim.Common
       var.SetValue(instance, value);
     }
 
-    [UsedImplicitly]
-    public static List<T> AllOf<T>() => Enum.GetValues(typeof(T)).OfType<T>().ToList();
+    public static GameObject Spawn(string prefabName, GameObject prefab, Vector3 location, Transform parent)
+    {
+      if (prefab == null) return null;
+      var instance = UnityEngine.Object.Instantiate(prefab, location, Quaternion.identity, parent);
+      return instance;
+    }
 
-    [UsedImplicitly]
-    public static bool IsGameInMainScene() => ZNetScene.instance != null;
-
-    [UsedImplicitly]
-    public static bool DoesPluginExist(string pluginGuid) => Chainloader.PluginInfos.Any(keyValuePair => keyValuePair.Value.Metadata.GUID == pluginGuid);
-
-    [UsedImplicitly]
-    public static string GetPluginPath(Type modPluginType) => Path.GetDirectoryName(modPluginType.Assembly.Location);
+    public static GameObject Spawn(GameObject prefab, Vector3 location, Transform parent)
+    {
+      Log.Trace(Logger, $"{Namespace}.{MethodBase.GetCurrentMethod().DeclaringType?.Name}.{MethodBase.GetCurrentMethod().Name}({prefab.name}, {location}, {parent.name})");
+      var instance = UnityEngine.Object.Instantiate(prefab, location, Quaternion.identity, parent);
+      return instance;
+    }
   }
 }
